6324ba7f8daa3f8a1eee6159f5e3b2e6df071338
Maria Brown <mariabrown0908@gmail.com>
2022-05-16 23:32:17 -0400

    updating home routes

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const { Project, User } = require('../models');
 
 router.get('/', (req, res) => {
     res.send('Hello Yall!');
 });
 
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const projectData = await Project.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+
   module.exports = router;
-  
